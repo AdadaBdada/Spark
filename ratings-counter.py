0424e049a281ccbf236d90779d7d0f84532dc927
Shuo <sjin6@stevens.edu>
2019-06-19 17:12:33 -0400

    add and delete

---
+++ ratings-counter.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+# configuration of spark
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+# load the u.data file from the MovieLens dataset
+lines = sc.textFile("./ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
